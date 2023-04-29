b4f441db4de996af96cbc34ebc79b8542611d2fe
zeromountain <sonyeongsan@gmail.com>
2023-04-30 02:56:38 +0900

    add socket using stompjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { CompatClient, IMessage, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import axios from 'axios';
+
+const ENDPOINT = process.env.REACT_APP_SOCKET_URL as string;
+
+interface Message {
+  content: string;
+  createdAt: string;
+  isImportant: boolean;
+  memoId: number;
+  modifiedAt: string;
+}
 
 function App() {
+  const client = useRef<CompatClient>();
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const loadMessages = () => {
+    axios
+      .get('/workspace/memo-list/1', {
+        baseURL: process.env.REACT_APP_API_URL,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setMessages(res.data);
+      });
+  };
+
+  const handleSubscribe = (message: IMessage) => {
+    // const newMessage = JSON.parse(message.body);
+    // console.log({ newMessage });
+    // setMessages((prevMessages) => [...prevMessages, newMessage.content]);
+  };
+
+  const sendHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    client.current?.send(
+      '/app/memo',
+      {},
+      JSON.stringify({
+        workspaceId: 1,
+        content: message,
+      })
+    );
+    setMessage('');
+    setTimeout(() => {
+      loadMessages();
+    }, 50);
+  };
+
+  const connectHandler = useCallback(() => {
+    // client.current = Stomp.over(() => {
+    //   const sock = new SockJS(ENDPOINT);
+    //   return sock;
+    // })
+    client.current = Stomp.over(() => new SockJS(ENDPOINT));
+    setMessages([]);
+    client.current.connect({}, () => {
+      console.log('asdfsadfsadfasgjklasdgklsjadgklasdjgklasdjgklsadjglasg');
+      loadMessages();
+      // client.current?.subscribe(`/sub/chat/room/${1}`, handleSubscribe);
+    });
+  }, []);
+
+  useEffect(() => {
+    connectHandler();
+    return () => {
+      client.current?.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chat App</h1>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message.content}</p>
+        ))}
+      </div>
+      <form onSubmit={sendHandler}>
+        <input
+          type='text'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   );
 }
